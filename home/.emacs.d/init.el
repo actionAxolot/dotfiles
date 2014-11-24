b9513cec2d92dfad27bdb637449837ba23f09a7b
aparato <yo@aparato.mx>
2014-11-24 11:47:18 -0600

    First commit

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,303 @@
+;; I need loop!! Should look for a way to remove it
+(require 'cl)
+
+;; This windows thing. God!!
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
+(setq-default default-buffer-file-coding-system 'utf-8-unix)
+
+;; Disable backups. I just hate the mess they make
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+
+;; hide splash screen
+(setq inhibit-startup-message t)
+
+;; Add more repos to package.el
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; Define default packages for installation
+(defvar axolote/packages '(
+			   paredit
+			   autopair
+			   magit
+			   smex
+               auto-complete
+			   helm
+               helm-ag
+               yasnippet
+               emmet-mode
+               web-mode
+               less-css-mode
+               fiplr
+               badger-theme
+               color-theme-sanityinc-tomorrow)
+  "Default packages")
+
+;; Install packages if not already
+(defun axolote/packages-installed-p ()
+  (loop for pkg in axolote/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (axolote/packages-installed-p)
+  (message "%s" "Refreshing package DB...")
+  (package-refresh-contents)
+  (dolist (pkg axolote/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Load vendor directory
+(defvar axolote/vendor-dir (expand-file-name "vendor" user-emacs-directory))
+(add-to-list 'load-path axolote/vendor-dir)
+
+(dolist (project (directory-files axolote/vendor-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+;; Set the custom theme
+(load-theme 'sanityinc-tomorrow-night t)
+
+
+;; Delete selected text... as God intended
+(delete-selection-mode t)
+(transient-mark-mode t)
+
+;; Font stuff
+;; (set-face-attribute 'default nil :height 100)
+
+;; (custom-set-variables '(linum-format (quote " %2d")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector ["black" "#E2434C" "#86B187" "#E0D063" "#84C452" "#E18CBB" "#8AC6F2" "white"])
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "97a2b10275e3e5c67f46ddaac0ec7969aeb35068c03ec4157cf4887c401e74b1" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "dc46381844ec8fcf9607a319aa6b442244d8c7a734a2625dac6a1f63e34bc4a6" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" "91b5a381aa9b691429597c97ac56a300db02ca6c7285f24f6fe4ec1aa44a98c3" "29a4267a4ae1e8b06934fec2ee49472daebd45e1ee6a10d8ff747853f9a3e622" "d293542c9d4be8a9e9ec8afd6938c7304ac3d0d39110344908706614ed5861c9" default)))
+ '(fci-rule-color "#343d46")
+ '(linum-format (quote dynamic))
+ '(org-agenda-files nil t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#bf616a") (40 . "#DCA432") (60 . "#ebcb8b") (80 . "#B4EB89") (100 . "#89EBCA") (120 . "#89AAEB") (140 . "#C189EB") (160 . "#bf616a") (180 . "#DCA432") (200 . "#ebcb8b") (220 . "#B4EB89") (240 . "#89EBCA") (260 . "#89AAEB") (280 . "#C189EB") (300 . "#bf616a") (320 . "#DCA432") (340 . "#ebcb8b") (360 . "#B4EB89"))))
+ '(vc-annotate-very-old-color nil))
+
+;; Make stuff easier
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; hide toolbar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; Proper line wrapping
+(global-visual-line-mode t)
+
+;; enable paredit useful buffers
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook  #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook #'enable-paredit-mode)
+
+;; Editing stuff
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; Keybindings
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region) ;; self explanatory
+
+;; Misc
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+(show-paren-mode t)
+
+;; SMEX
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; IDO
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+
+;; Column number mode
+(setq column-number-mode t)
+
+;; autopair
+(require 'autopair)
+(add-hook 'web-mode-hook (lambda ()
+                           (setq autopair-dont-activate) t))
+
+;; configure helm
+(require 'helm-config)
+
+(global-set-key (kbd "C-c h") 'helm-mini)
+
+;; Configure YASnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Smart beginning of line
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character"
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
+(global-set-key (kbd "C-a") 'smart-beginning-of-line)
+
+;; To be honest... not having a newline on enter sucks
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; uniquify... which I kind of remember was already set...
+(require 'uniquify)
+(setq
+ uniquify-buffer-name-style 'post-forward
+ uniquify-separator ":")
+
+;; Activate Emmet mode on shit
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'html-mode 'emmet-mode)
+
+;; Close matching stuff like braces
+(autopair-global-mode)
+
+;; LANGUAGE CONFIGURATIONS
+
+;; PYTHON
+;; Virtualenv settings
+;; (require 'virtualenvwrapper)
+;; (venv-initialize-interactive-shells)
+;; (venv-initialize-eshell)
+;; (setq venv-location "~/.virtualenvs/")
+
+;; HTML, JS, CSS
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; Org-Mode and shit
+; Bindings and stuff
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+
+
+
+; Todo flow and stuff
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "white" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
+
+(setq org-use-fast-todo-selection t)
+(setq org-treat-S-cursor-todo-selection-as-state-change nil)
+
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("HOLD" ("WAITING" . t) ("HOLD" . t))
+              (done ("WAITING") ("HOLD"))
+              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+
+
+;; Define the path to stuff depending on whether the editor is used in
+;; windows or a *NIX environment
+(setq org-base-path (expand-file-name "~/Dropbox/journal/"))
+(when (eq system-type 'windows-nt)
+  (setq org-base-path "C:/Users/Mario/Dropbox/journal/"))
+
+; notes file
+(setq org-default-notes-file (format "%s/%s" org-base-path "notes.org"))
+
+;; This is so that it works with the android app
+(setq org-mobile-directory (format "%s/%s" org-base-path "mobile"))
+(setq org-directory org-base-path)
+
+; Agenda stuff
+;;(setq org-agenda-files (quote ((format "%s" org-base-path))))
+(setq org-agenda-files (list org-base-path (format "%s/%s" org-base-path "clients")))
+(setq org-mobile-files (list org-base-path (format "%s/%s" org-base-path "clients")))
+
+(setq org-mobile-inbox-for-pull (format "%s/%s/%s" org-base-path "inbox" "inbox.org"))
+
+; Org capture templates
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree (format "%s/%s" org-base-path "diary.org"))
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file (format "%s/%s" org-base-path "refile.org"))
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"<%Y-%m-%d %a .+1d/3d>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
+;; Habit tracking
+(setq org-log-repeat "time")
+
+;; Fiplr
+(global-set-key (kbd "C-x f") 'fiplr-find-file)
+
+;; Windmove and stuff
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+;; Jedi settings
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:setup-keys t)
+;; (setq jedi:complete-on-dot t)
+
+;; Autocomplete settings
+(require 'auto-complete-config)
+(ac-config-default)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
+(put 'dired-find-alternate-file 'disabled nil)
+
+
+
---
+++ home/.vim/.vimrc
@@ -0,0 +1,223 @@
+"--- Turn off vi compatibility ---"
+set nocompatible
+
+"---fucking line endings
+set ffs=unix
+
+"--- Have jj escape insert mode ---"
+inoremap jj <Esc>
+
+"---- Color in terminal ----"
+if has("unix") || has("mac")
+    if has('vim_starting')
+        set runtimepath+=~/.vim/bundle/neobundle.vim/
+    endif
+
+    call neobundle#begin(expand('~/.vim/bundle/'))
+else
+    if has('vim_starting')
+        set runtimepath+=~/vimfiles/bundle/neobundle.vim/
+    endif
+
+    call neobundle#begin(expand('~/vimfiles/bundle/'))
+endif
+
+"------- Packages to use ------"
+NeoBundleFetch 'Shougo/neobundle.vim'
+NeoBundle 'vim-scripts/delimitMate.vim.git'
+NeoBundle 'vim-scripts/Gundo.git'
+NeoBundle 'vim-scripts/L9.git'
+NeoBundle 'scrooloose/nerdcommenter.git'
+NeoBundle 'scrooloose/nerdtree.git'
+NeoBundle 'tpope/vim-surround.git'
+NeoBundle 'vim-scripts/Tagbar.git'
+NeoBundle 'mattn/emmet-vim.git'
+NeoBundle 'tpope/vim-fugitive.git'
+NeoBundle 'terryma/vim-multiple-cursors'
+NeoBundle 'SirVer/ultisnips'
+NeoBundle 'honza/vim-snippets'
+NeoBundle 'Shougo/unite.vim'
+NeoBundle 'Shougo/vimproc.vim'
+NeoBundle 'groenewege/vim-less'
+NeoBundle 'whatyouhide/vim-gotham'
+NeoBundle 'bling/vim-airline'
+NeoBundle 'xolox/vim-misc'
+NeoBundle 'xolox/vim-notes'
+
+
+
+if has("unix") || has("mac")
+    NeoBundle 'davidhalter/jedi-vim'
+endif
+
+call neobundle#end()
+
+syntax on
+filetype on
+filetype plugin indent on
+
+"------ Vim-notes configuration
+let g:notes_directories = ['~/Dropbox/notes']
+
+
+"--- easytags ---"
+" let g:easytags_cmd = "/usr/local/Cellar/ctags/5.8/bin/ctags"
+" set tags=./tags;
+" let g:easytags_dynamic_files = 1
+
+"--- GUI window configurations ---"
+set winaltkeys=no
+set guioptions-=T
+set guioptions-=r
+set guioptions-=L
+
+if has("mac")
+    set guifont=Monaco\ Mono\ 10
+elseif has("unix")
+    set guifont=Ubuntu\ Mono\ 10
+else
+    set guifont=Inconsolata:h11
+endif
+
+
+"---Remap key---"
+let mapleader = ","
+
+"---Fold Method---"
+set foldmethod=indent
+set foldlevel=99
+
+"---compatibility and security---"
+set modelines=0
+set ttimeoutlen=100
+
+"--- Airport conf ----"
+"let g:airline#extensions#tabline#enabled = 1
+
+"---Set Color Scheme---"
+colorscheme gotham256
+
+"---Tabs configuration---"
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+
+"---Vim editor settings---"
+set modelines=5
+set mousemodel=popup
+set pastetoggle=<F2>
+set encoding=utf-8
+set scrolloff=3
+set autoindent
+set showmode
+set showcmd
+set hidden
+set wildmenu
+set wildmode=longest:full,full
+set cursorline
+set ruler
+set backspace=indent,eol,start
+set laststatus=2
+set number
+set nobackup
+set noswapfile
+
+
+"---Vim search improvements---"
+nnoremap / /\v
+vnoremap / /\v
+set ignorecase
+set smartcase
+set gdefault
+set incsearch
+set showmatch
+set hlsearch
+nnoremap <leader><space> :noh<CR>
+
+"---Vim Longlines mess---"
+set textwidth=100
+set formatoptions=qrn1
+
+"---Mappings to show hidden characters---"
+nmap <leader>l :set list!<CR>
+set listchars=tab:▸\ ,eol:¬
+
+"---Match it configuration---"
+runtime macros/matchit.vim
+
+"---filetype identification---"
+autocmd FileType javascript setlocal omnifunc=javascriptcomplete#CompleteJS
+autocmd FileType html setlocal omnifunc=htmlcomplete#CompleteTags
+autocmd FileType css setlocal omnifunc=csscomplete#CompleteCSS
+autocmd FileType xml setlocal omnifunc=xmlcomplete#CompleteTags
+autocmd FileType php setlocal omnifunc=phpcomplete#CompletePHP
+autocmd FileType c setlocal omnifunc=ccomplete#Complete
+set completeopt=menuone,longest,preview,menu
+
+"---Indentation according to filetype---"
+autocmd FileType ruby set tabstop=2|set shiftwidth=2|set noexpandtab
+autocmd FileType python set tabstop=4|set shiftwidth=4|set expandtab
+
+"---Working with windows---"
+set winminheight=0
+nnoremap <leader>wv <C-w>v<C-w>l
+nnoremap <leader>ws <C-w>s<C-w>l
+nnoremap <C-h> <C-w>h
+nnoremap <C-j> <C-w>j
+nnoremap <C-k> <C-w>k
+nnoremap <C-l> <C-w>l
+
+"---Keybind NerdTree---"
+map <leader>nt :NERDTreeToggle<CR>
+
+"---keybind for tagbar---"
+map <leader>tb :TagbarToggle<CR>
+
+"--- Gundo mappings ---"
+map <leader>g :GundoToggle<CR>
+
+"---Store Backup files in a central place---"
+set backup
+set backupdir=~/tmp
+set dir=~/tmp
+
+"------ Misc. Bindings -----"
+"- Codefolding remap -"
+nnoremap <space> za
+
+"--- Create mappings to edit and source vimrc ---"
+nmap <leader>vr :tabedit $MYVIMRC<CR>
+
+" --- Unite and stuff ---"
+let g:unite_source_history_yank_enable = 1
+call unite#filters#matcher_default#use(['matcher_fuzzy'])
+if has("win32")
+    nnoremap <leader>t :<C-u>Unite -no-split -buffer-name=files   -start-insert file_rec<cr>
+else
+    nnoremap <leader>tt :<C-u>Unite -no-split -buffer-name=files   -start-insert file_rec/async<cr>
+    nnoremap <leader>a :Unite grep:.<cr>
+endif
+nnoremap <leader>f :<C-u>Unite -no-split -buffer-name=files   -start-insert file<cr>
+nnoremap <leader>r :<C-u>Unite -no-split -buffer-name=mru     -start-insert file_mru<cr>
+nnoremap <leader>o :<C-u>Unite -no-split -buffer-name=outline -start-insert outline<cr>
+nnoremap <leader>y :<C-u>Unite -no-split -buffer-name=yank    history/yank<cr>
+nnoremap <leader>e :<C-u>Unite -no-split -buffer-name=buffer  buffer<cr>
+
+" Custom mappings for the unite buffer
+autocmd FileType unite call s:unite_settings()
+function! s:unite_settings()
+  " Enable navigation with control-j and control-k in insert mode
+  imap <buffer> <C-j>   <Plug>(unite_select_next_line)
+  imap <buffer> <C-k>   <Plug>(unite_select_previous_line)
+endfunction
+
+"--- clipboard this shit
+set clipboard=unnamedplus
+
+
+" Ultisnips conf
+let g:UltiSnipsExpandTrigger="<tab>"
+
+" ---- Check for uninstalled bundles
+NeoBundleCheck
